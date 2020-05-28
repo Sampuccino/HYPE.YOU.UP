acc4181f9bf640182f664f177f4a50c71e6c0131
Sampuccino <Sampuccino@gmail.com>
2020-05-28 14:47:54 -0700

    Create Database Schemas. Closes #4.

---
+++ database/migrations/2020_05_28_211248_create_reviews_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('reviews', function (Blueprint $table) {
+          $table->increments('id');
+          $table->timestamps();
+          $table->unsignedInteger('reviewee_id');
+          $table->integer('pt_score'); // PT-Score is the personality type score on a given personality type. +/- 5 based @ Personality Chosen for Reviewee
+          $table->integer('q1');
+          $table->integer('q2');
+          $table->integer('q3');
+          $table->integer('q4');
+          $table->integer('q5');
+          $table->integer('q6');
+          $table->integer('q7');
+          $table->integer('q8');
+          $table->integer('q9');
+          $table->integer('q10');
+          $table->text('pros');
+          $table->text('cons');
+          $table->text('snapchat_url');
+          $table->text('facebook_url');
+          $table->text('instagram_url');
+      });
+
+      // Relationship
+      Schema::table('reviews', function($table) {
+        $table->foreign('reviewee_id')->references('id')->on('reviewees')->onDelete('cascade');
+      });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
---
+++ database/migrations/2020_05_28_213752_create_comments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedInteger('review_id');
+            $table->string('name', 25);
+            $table->text('image');
+            $table->text('body');
+            $table->double('rating'); // Used for aggregating total rating score
+            $table->string('recommend'); // Used for % score on Profile
+        });
+
+      // Relationship
+      Schema::table('comments', function($table) {
+        $table->foreign('review_id')->references('id')->on('reviews')->onDelete('cascade');
+      });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
