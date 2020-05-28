2688ec42f97f258cad8e990760ec3f2a67c7bbc1
Sampuccino <Sampuccino@gmail.com>
2020-05-28 15:34:09 -0700

    Create Models & Controllers. Closes #5.

---
+++ app/Http/Controllers/RevieweeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reviewee;
+use Illuminate\Http\Request;
+
+class RevieweeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Reviewee  $reviewee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reviewee $reviewee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Reviewee  $reviewee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reviewee $reviewee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Reviewee  $reviewee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reviewee $reviewee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Reviewee  $reviewee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reviewee $reviewee)
+    {
+        //
+    }
+}
